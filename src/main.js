1e5041e2ff4c188e3610449855b17ff897630e87
Rasmus Stenlund <rasmus.stenlund@gmail.com>
2026-08-07 00:15:59 +0200

    Add date input, restructure html and add basic styling

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 
 const container = document.querySelector('#app')
-
-container.innerHTML = '<p>Loading</p>'
+const title_container = document.getElementById('nasa-title')
+const image_container = document.getElementById('nasa-image')
+const explanation_container = document.getElementById('nasa-explanation')
+const loading = document.getElementById('nasa-loading')
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
   .then(response => response.json())
@@ -10,7 +12,7 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
     let media;
 
     if (data.media_type === "image") {
-      media = `<img src = '${data.url}' style = 'height: 400px;'>`;
+      media = `<img src = '${data.url}'>`;
     } else if (data.url.includes('youtube')) {
       let embed_url = data.url.replace('watch?v=', 'embed/')
       media = `<iframe src = '${embed_url}' style = 'width: 640px; height: 360px;'></iframe> `
@@ -18,12 +20,11 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
       media = `<video src = '${data.url}' controls></video>`;
     }
 
-    container.innerHTML = `
-      <p>${data.date}</p>
-      <h2>${data.title}</h2>
-      ${media}
-      <P>${data.explanation}</p>
-    `;
+    loading.classList.add('hidden')
+
+    title_container.innerHTML = `<h2>${data.title}</h2>`;
+    image_container.innerHTML = `${media}`;
+    explanation_container.innerHTML = `<p>${data.explanation}</p>`;
   })
   .catch(err => {
     container.innerHTML = `<p>Error: ${err.message}</p>>`
