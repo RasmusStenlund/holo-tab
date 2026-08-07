f4ac8e873781764adb114db44ffdc9eff38e7f88
RasmusStenlund <rasmus.stenlund@gmail.com>
2026-08-07 22:01:59 +0200

    Restructure main, reduce load time with cache and direct connection

--- src/main.js
+++ src/main.js
@@ -31,16 +31,16 @@ const image_container = document.getElementById('nasa-image')
 const explanation_container = document.getElementById('nasa-explanation')
 const loading = document.getElementById('nasa-loading')
 
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-.then(response => response.json())
-.then(data => {
+const APODCache = {}
+
+function show_apod(data) {
   let media;
 
   if (data.media_type === "image") {
     media = `<img src = '${data.url}'>`;
   } else if (data.url.includes('youtube')) {
     let embed_url = data.url.replace('watch?v=', 'embed/')
-    media = `<iframe src = '${embed_url}' style = 'width: 640px; height: 360px;'></iframe> `
+    media = `<iframe src = '${embed_url}' style = 'width: 640px; height: 360px;'></iframe>`
   } else {
     media = `<video src = '${data.url}' controls></video>`;
   }
@@ -56,60 +56,56 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
   date_container.classList.remove('hidden');
   image_container.classList.remove('hidden');
   explanation_container.classList.remove('hidden');
-})
-.catch(err => {
-  loading.classList.add('hidden');
-  error_container.innerHTML = `<p>Error: ${err.message}</p>>`
-  error_container.classList.remove('hidden')
-})
+}
 
-const date_input = document.getElementById('date-picker')
-
-const today = new Date().toISOString().split('T')[0]
-
-date_input.max = today;
-date_input.value = today;
-
-const confirm_date = document.getElementById('confirm-date')
-
-confirm_date.addEventListener('click', function () {
+function get_apod(date = '') {
   loading.classList.remove('hidden');
   error_container.classList.add('hidden');
   title_container.classList.add('hidden');
   date_container.classList.add('hidden');
   image_container.classList.add('hidden');
   explanation_container.classList.add('hidden');
+  
+  let date_add = ''
+  if (date) {
+    date_add = `&date=${date}`
+  }
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}${date_add}`
 
-  fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date_input.value}`)
-  .then(response => response.json())
-  .then(data => {
-    let media;
+  if (APODCache[url]) {
+    show_apod(APODCache[url]);
+    return;
+  }
 
-    if (data.media_type === "image") {
-      media = `<img src = '${data.url}'>`;
-    } else if (data.url.includes('youtube')) {
-      let embed_url = data.url.replace('watch?v=', 'embed/')
-      media = `<iframe src = '${embed_url}' style = 'width: 640px; height: 360px;'></iframe> `;
-    } else {
-      media = `<video src = '${data.url}' controls></video>`;
+  fetch(url)
+  .then(response =>  {
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText} - Please try again.`);
     }
-
-    loading.classList.add('hidden');
-    error_container.classList.add('hidden');
-
-    title_container.innerHTML = `<h2>${data.title}</h2>`;
-    date_container.innerHTML = `<p>${data.date}</p>`;
-    image_container.innerHTML = `${media}`;
-    explanation_container.innerHTML = `<p>${data.explanation}</p>`;
-    title_container.classList.remove('hidden');
-    date_container.classList.remove('hidden')
-    image_container.classList.remove('hidden');
-    explanation_container.classList.remove('hidden');
+    return response.json();
+  })
+  .then(data => {
+    APODCache[url] = data
+    show_apod(data)
   })
   .catch(err => {
     loading.classList.add('hidden');
-    error_container.innerHTML = `<p>Error: ${err.message}</p>>`;
-    error_container.classList.remove('hidden');
+    error_container.innerHTML = `<p>Error: ${err.message}</p>`
+    error_container.classList.remove('hidden')
   })
+}
+
+get_apod()
+
+
+const date_input = document.getElementById('date-picker')
+const today = new Date().toISOString().split('T')[0]
+date_input.max = today;
+date_input.value = today;
+
+const confirm_date = document.getElementById('confirm-date')
+
+confirm_date.addEventListener('click', function () {
+  get_apod(date_input.value);
 })
 
